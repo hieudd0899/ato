94a23f998766fbfe3e9e700759fc16ecec5b3e17
hieudd <hieuduong@asmingroup.vn>
2025-01-14 11:22:50 +0700

    ADMIN: Turn off SEO for admin site

--- packages/b2c-admin/next.config.mjs
+++ packages/b2c-admin/next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ['common'],
+    async headers() {
+        const header = [];
+
+        header.push({
+            headers: [
+                {
+                    key: 'X-Robots-Tag',
+                    value: 'noindex',
+                },
+            ],
+            source: '/:path*',
+        });
+
+        return header;
+    },
 };
 
 export default nextConfig;
