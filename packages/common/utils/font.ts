34198d882d29fbefdcbf8f796a93e4bd7bf3a3a2
hieudd <hieuduong@asmingroup.vn>
2025-01-20 08:17:14 +0700

    Codebase: Config global font

--- packages/common/utils/font.ts
+++ packages/common/utils/font.ts
@@ -2,7 +2,7 @@ import { Open_Sans } from 'next/font/google';
 
 export const PRIMARY_FONT = Open_Sans({
     subsets: ['latin'],
-    weight: ['400', '500', '600', '700'],
+    weight: ['400', '500', '600', '700', '800'],
     display: 'swap',
     adjustFontFallback: false,
 });
